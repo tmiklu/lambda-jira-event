ed75f554d901770147d7a6be4be6c1c244a389c2
tmiklu <61796331+tmiklu@users.noreply.github.com>
2021-03-29 10:29:33 +0200

    Update jira.py

--- jira.py
+++ jira.py
@@ -1,67 +1,66 @@
 import json
 import boto3
-from botocore.vendored import requests
+import urllib3
 
 client = boto3.client('codebuild')
 ssm    = boto3.client('ssm')
 
-
+              
 def lambda_handler(event, context):
     # TODO implement
-    print(event['body'])
+    #print(event['body'])
     if event['httpMethod'] == 'POST':
+      event_body = event['body']
+      main_event = json.loads(event_body)
+      
+      #print(main_event)
+      
+      changelog_exist = 'changelog' in main_event
+      
+      if changelog_exist:
+        print(changelog_exist)
         
-        json_event      = event['body']
-        get_event       = json.loads(json_event)
+        fix_version  = main_event['issue']['fields']['fixVersions'][0]['name']
+        status_field = main_event['changelog']['items'][0]['field']
+        from_id      = main_event['changelog']['items'][0]['from']
+        to_id        = main_event['changelog']['items'][0]['to']
         
-        try:
-          fix_version     = get_event['issue']['fields']['fixVersions'][0]['name']
+        if status_field == 'status' and from_id == '12801' and to_id == '3':
           
-          #repo_url        = get_event['issue']['fields']['customfield_18209']
-          #source_type     = get_event['issue']['fields']['customfield_18211']['value']
-          
-          status_field    = get_event['changelog']['items'][0]['field']
-          from_id         = get_event['changelog']['items'][0]['from']
-          to_id           = get_event['changelog']['items'][0]['to']
-          
-        except:
-          print('Exception occured. No build required')
-        
-        else:
-          if status_field == 'status' and from_id == '12801' and to_id == '3':
-
-            for issue_links in get_event['issue']['fields']['issuelinks']:
+          for issue_links in main_event['issue']['fields']['issuelinks']:
+            
+            if issue_links['type']['id'] == '10334' and issue_links['outwardIssue']['fields']['issuetype']['id'] == '10358':
+  
+              print(issue_links)
               
-              try:
-                issue_links['type']['id'] == '10334'
-                issue_links['outwardIssue']['fields']['issuetype']['id'] == '10358'
-                
-                if issue_links['type']['id'] == '10334' and issue_links['outwardIssue']['fields']['issuetype']['id'] == '10358':
-                  
-                  print(issue_links['outwardIssue']['self'])
-                  
-                  username = ssm.get_parameter(
-                      Name='jira-credentials-username',
-                      WithDecryption=False
-                  )
-                  
-                  password = ssm.get_parameter(
-                      Name='jira-credentials-api',
-                      WithDecryption=False
-                  )
-                  
-                  url      = 'https://xxxx.xxx.xxx/rest/api/2/search?jql=fixversion=xx_xxx-x.x.x'
-                  user     = username['Parameter']['Value']
-                  passwd   = password['Parameter']['Value']
+              issue_id = issue_links['outwardIssue']['id']
               
-                  #x = requests.get(url, auth = (user, passwd))
-                  #print(x.json())
+              username = ssm.get_parameter(
+                Name           = 'jira-credentials-username',
+                WithDecryption = False
+                )
+                                        
+              password = ssm.get_parameter(
+                Name           = 'jira-credentials-api',
+                WithDecryption = True
+                )
               
-              except:
-                print('Exception occured. No build required')
-
-        
+              
+              http     = urllib3.PoolManager(cert_reqs='CERT_NONE')
+              
+              url      = 'https://jira.dtvlaops.net/rest/api/2/issue/' + issue_id + '/'
+              user     = username['Parameter']['Value']
+              passwd   = password['Parameter']['Value']
+              
+              
+              authHeader = urllib3.make_headers(basic_auth=user + ':' + passwd)
+              resp       =  http.request('GET', url, headers=authHeader)
+              return json.loads(resp.data.decode('utf-8'))
+      
+      else:
+        print('Changelog object does not exists. No build required.')
+  
     else:
       return {
               'statusCode': 405
-        }
+      }
